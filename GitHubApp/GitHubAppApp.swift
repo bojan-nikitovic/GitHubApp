f6f179d2fcbc36ff5b444ec4200627df5d4f1be6
Bojan Nikitovic <bojan@bojans-Macbook-Pro.local>
2024-04-28 15:04:43 +0200

    Add empty project

---
+++ GitHubApp/GitHubAppApp.swift
@@ -0,0 +1,14 @@
+//
+//  Created by Bojan on 28.4.24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            
+        }
+    }
+}
